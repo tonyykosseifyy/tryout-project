7a479885470bdcafc3e490c1a808b4d4d3c31f23
tonyykosseifyy <tonykosseify123@gmail.com>
2021-09-20 14:30:17 +0300

    creating the srevre

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
-const express = require('express') ;
+import express from 'express' ;
+import cors from 'cors' ;
 
 
 const app = express() ;
+
+app.use(cors()) ;
+app.use(express.json()) ;
 
 
 const port = process.env.PORT || 3000 ;
