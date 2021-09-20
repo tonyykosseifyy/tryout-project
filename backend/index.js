968e1cc642f017e12c07e349667107940f45c2ac
tonyykosseifyy <tonykosseify123@gmail.com>
2021-09-20 14:49:53 +0300

    creating the api

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,20 @@ import cors from 'cors' ;
 
 const app = express() ;
 
-app.use(cors()) ;
-app.use(express.json()) ;
+app.use(express.json({ limit: "500" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 
 const port = process.env.PORT || 3000 ;
 
+
+app.get("/api" , ( req , res) => {
+    res.status(200).send({
+        "name" : "tony kosseify" ,
+        "email" : "tonykosseify123@gmail.com"
+    });
+});
 
 app.listen( port , () => {
     console.log(`Server is listening on port ${port}`) ;
